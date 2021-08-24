fabf281fc94acfc6fcec99cb79cd0fcad66fa51a
SH H <hyn41@likelion.org>
2021-08-24 14:57:55 +0900

    8/24 (class 5ban commit 1)

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
@@ -11,21 +12,27 @@ from subscribeapp.models import Subscription
 
 @method_decorator(login_required, 'get')
 class SubscriptionView(RedirectView):
+
     def get(self, request, *args, **kwargs):
         user = request.user
         project = Project.objects.get(pk=kwargs['project_pk'])
+
         subscription = Subscription.objects.filter(user=user,
                                                    project=project)
+
         if subscription.exists():
             subscription.delete()
         else:
             Subscription(user=user, project=project).save()
+
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
         return reverse('projectapp:detail', kwargs={'pk': kwargs['project_pk']})
 
 
+
+@method_decorator(login_required, 'get')
 class SubscriptionListView(ListView):
     model = Article
     context_object_name = 'article_list'
